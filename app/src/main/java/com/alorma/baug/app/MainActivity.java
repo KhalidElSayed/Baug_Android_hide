7e40c279bf96110dae35463068dfe8b62c86d7e0
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-04-08 15:48:19 +0200

    Base

--- app/src/main/java/com/alorma/baug/app/MainActivity.java
+++ app/src/main/java/com/alorma/baug/app/MainActivity.java
@@ -2,14 +2,42 @@ package com.alorma.baug.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.action1).setOnClickListener(this);
+        findViewById(R.id.action2).setOnClickListener(this);
+        findViewById(R.id.action3).setOnClickListener(this);
+
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.action1:
+                showResult("Action 1");
+                break;
+            case R.id.action2:
+                showResult("Action 2");
+                break;
+            case R.id.action3:
+                showResult("!!!!! Action 3 !!!!!");
+                break;
+        }
+    }
+
+    private void showResult(String message) {
+        textView.setText(message);
     }
 }
